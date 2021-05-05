db74230760296166d2418e01db0603c3fdf33b9c
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-05 11:03:33 +0530

    Refactor using Generics

---
+++ MaximumNumberGenerics/GenericMaximum.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNumberGenerics
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+        public GenericMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+        public T[] value;
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        void add(params int[] a)
+        {
+
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+
+            return sorted_values[sorted_values.Length - 1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+    }
+}
--- MaximumNumberGenerics/Program.cs
+++ MaximumNumberGenerics/Program.cs
@@ -12,13 +12,22 @@ namespace MaximumNumberGenerics
         {
             Console.WriteLine("Welcome to the Maximum Number Program using Generics");
             Console.WriteLine("Find max Number");
-            MaximumNumberCheck MaxInt = new MaximumNumberCheck();
-            int output = MaxInt.MaximumIntegerNumber(11, 22, 33);
-            Console.WriteLine(output);
-            float floatoutput = MaxInt.MaximumFloatNumber(111.2f, 22.45f, 78.56f);
-            Console.WriteLine(floatoutput);
-            string stringoutput = MaxInt.MaximumStringNumber("22", "33", "44");
-            Console.WriteLine(stringoutput);
+            //MaximumNumberCheck MaxInt = new MaximumNumberCheck();
+            //int output = MaxInt.MaximumIntegerNumber(11, 22, 33);
+            //Console.WriteLine(output);
+            //float floatoutput = MaxInt.MaximumFloatNumber(111.2f, 22.45f, 78.56f);
+            //Console.WriteLine(floatoutput);
+            //string stringoutput = MaxInt.MaximumStringNumber("22", "33", "44");
+            //Console.WriteLine(stringoutput);
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "111", "222", "333", "55", "999" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
         }
     }
 }
