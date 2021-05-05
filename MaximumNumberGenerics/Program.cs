972abfe4e89ebefea558d2b2adda44e29f72e06a
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-05 10:48:05 +0530

    Add project files.

---
+++ MaximumNumberGenerics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNumberGenerics
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to MaximumTest using Generics!");
+        }
+    }
+}
